e986254640d3543380d4ffc5814ec2be7ae773e4
ankh04 <ankh04@icloud.com>
2022-09-12 14:29:33 +0800

    增加数据单元，并使用数组重构

---
+++ 日程表/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  日程表
+//
+//  Created by 张环宇 on 2022/9/12.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
--- 日程表/ChecklistViewController.swift
+++ 日程表/ChecklistViewController.swift
@@ -8,35 +8,63 @@
 import UIKit
 
 class ChecklistViewContoller: UITableViewController {
+    
+    var items = [ChecklistItem]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let item0 = ChecklistItem()
+        item0.text = "遛狗"
+        items.append(item0)
+        
+        let item1 = ChecklistItem()
+        item1.text = "刷牙"
+        item1.checked = true
+        items.append(item1)
+        
+        let item2 = ChecklistItem()
+        item2.text = "学习"
+        items.append(item2)
+        
+        let item3 = ChecklistItem()
+        item3.text = "运动"
+        item3.checked = true
+        items.append(item3)
+        
+        let item4 = ChecklistItem()
+        item4.text = "吃冰激凌"
+        items.append(item4)
+    }
+    
+    // MARK: - Help Functions
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
     }
     
     // MARK: - Table View Data Source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
-    
-    // TODO: 试试看
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "checklistitem", for: indexPath)
-        // FIXME: ddd
-        let label = cell.viewWithTag(1000) as! UILabel
         
-        if indexPath.row == 0 {
-            label.text = "遛狗"
-        } else if indexPath.row == 1 {
-            label.text = "刷牙"
-        } else if indexPath.row == 2 {
-            label.text = "学习"
-        } else if indexPath.row == 3 {
-            label.text = "锻炼"
-        } else if indexPath.row == 4 {
-            label.text = "吃冰激淋"
-        }
+        let item = items[indexPath.row]
+        
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         
         return cell
     }
@@ -44,11 +72,10 @@ class ChecklistViewContoller: UITableViewController {
     // MARK: - Table View Delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            if cell.accessoryType == .none {
-                cell.accessoryType = .checkmark
-            } else {
-                cell.accessoryType = .none
-            }
+            let item = items[indexPath.row]
+            item.checked.toggle()
+            
+            configureCheckmark(for: cell, with: item)
         }
         tableView.deselectRow(at: indexPath, animated: true)
         
