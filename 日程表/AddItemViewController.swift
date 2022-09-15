995904c87dcbc381bbc330cfa0ffef0189f49bf4
ankh04 <ankh04@icloud.com>
2022-09-15 12:25:10 +0800

    添加日程页面增加输入框 优化输入框

--- 日程表/AddItemViewController.swift
+++ 日程表/AddItemViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
     
     // MARK: - Actions
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
     }
     @IBAction func done() {
+        // 与右上角的“确定”绑定
+        // 与键盘的“Done”绑定
+        print("输入框的内容是: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
 
@@ -21,6 +26,35 @@ class AddItemViewController: UITableViewController {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
 
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // 在view controller渲染之前，这个函数会被调用
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 相当于 focus
+        textField.becomeFirstResponder()
+    }
+    
+    // MARK: - Txet Fild Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
     
 }
